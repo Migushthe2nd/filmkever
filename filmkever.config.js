88104155ff57a5de26919fcc4b7e9530051771bb
Migushthe2nd <Migushthe2nd@users.noreply.github.com>
2020-03-26 11:42:52 +0100

    Fix linting

--- filmkever.config.js
+++ filmkever.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'filmkever',
-    script: './server/index.js',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-};
+    apps: [
+        {
+            name: 'filmkever',
+            script: './server/index.js',
+            instances: 1,
+            autorestart: true,
+            watch: true,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+}
