4a131f3a856117726d0dc3c849f674b49d382c2b
Migushthe2nd <Migushthe2nd@users.noreply.github.com>
2020-04-02 12:14:34 +0200

    Split communication in mixin

---
+++ web_utils/communicateApp.js
@@ -0,0 +1,39 @@
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        receiveMessage(event) {
+            if (event.origin !== 'null') return
+
+            const sendPosition = () => {
+                this.$parent.mediaProgress(
+                    this.$parent.mediatype === 'movie' ? 'movie' : 'episode',
+                    this.$parent.currPlayingTraktID,
+                    (event.data.position / event.data.length) * 100 >
+                        this.$store.state.user.preferences.finishPercentage
+                        ? true
+                        : null,
+                    event.data.length,
+                    event.data.position
+                )
+                this.lastPosition = event.data.position
+            }
+
+            switch (event.data.name) {
+                case 'position':
+                    if (this.$store.state.loggedIn) {
+                        if (
+                            this.selectedSource.name === 'MovieFiles' &&
+                            event.data.position > this.lastPosition
+                        ) {
+                            sendPosition()
+                        } else {
+                            sendPosition()
+                        }
+                    }
+                    break
+            }
+        }
+    }
+}
